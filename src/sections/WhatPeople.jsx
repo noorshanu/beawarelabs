4a37bb9d4270bdda248b5e392953ddc1ed27c1e2
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-26 03:58:31 +0530

    new

--- src/sections/WhatPeople.jsx
+++ src/sections/WhatPeople.jsx
@@ -2,7 +2,22 @@
 
 function WhatPeople() {
   return (
-    <div>WhatPeople</div>
+    <section className="bg-[#f4f4f4] w-full relative">
+    <div className="flex flex-wrap px-[60px] py-[60px] mx-auto my-0 max-w-[1440px]">
+    <div className="flex flex-col w-full">
+    <h3 className="text-center text-black text-[40px] font-[600] mb-[60px]">
+    What people say about us
+          </h3>
+        </div>
+    <div>
+        <div className="people-row flex min-w-0 -ml-[15px] -mr-[15px] ">
+
+        </div>
+    </div>
+
+    </div>
+    
+    </section>
   )
 }
 
