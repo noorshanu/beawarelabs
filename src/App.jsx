ada63bf87bc910b8278069394600f1b1a218dc04
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-25 20:28:52 +0530

    repo created some basic style

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
