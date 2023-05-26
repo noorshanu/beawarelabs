0c12d890bdd6f340906b60c1c19c3a458a669e0b
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-26 15:22:15 +0530

    Update NewsLetter.jsx

--- src/sections/NewsLetter.jsx
+++ src/sections/NewsLetter.jsx
@@ -10,10 +10,10 @@ function NewsLetter() {
           </h3>
             <p className="font-[16px] text-[#313131] text-center  mt-0 mx-0 mb-7">
                 Get updates on our latest news and product releases.</p>
-                <form action="" className="flex text-left ant-form">
+                <form action="" className="flex text-left ant-form gap-5">
                     <div className="ant-form-item m-0">
-                        <div className="flex text-sm max-w-0 ant-row">
-                            <div className="ant-col ant-form-item-control w-full max-w-0 flex flex-col">
+                        <div className="flex text-sm min-w-0 ant-row">
+                            <div className="ant-col ant-form-item-control w-full flex flex-col">
                                 <div className="relative flex items-center min-h-[32px]">
                                     <div className="flex-auto max-w-[100%]">
                                     <input placeholder="Enter your email address" type="email" id="email" aria-required="true" className="ant-input " value=""/>
@@ -25,6 +25,7 @@ function NewsLetter() {
 
                         </div>
                     </div>
+                    <button type="button" className="text-white bg-[#212121] hover:bg-[#fdd46e] hover:text-black sm:w-[150px] w-[300px] h-12 flex justify-center items-center font-[600] text-[15px] rounded-md border-0  ">Subscribe</button>
                 </form>
         </div>
     </section>
