4c776e2498fb7d361a4217046cffaa922c330df3
unknown <anas.452111@gmail.com>
2023-06-08 23:15:18 +0500

    Tables updated || Airdrop sidebar Completed

--- src/components/AirdropSidebar.jsx
+++ src/components/AirdropSidebar.jsx
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import chat from '../../utilts/chat.json'
+import {BsEmojiSmile} from 'react-icons/bs'
+
+
+const ChatMessage = ({ sender, date, image = null, text = '', profilePic }) => {
+  return (
+    <div className="w-full flex">
+      <div className="rounded-full h-6 w-6 mr-2">
+        <img src={profilePic} alt={'Sender'} className="rounded-full h-6 w-6" />
+      </div>
+      <div className="flex-1 pr-3">
+        <div className="flex w-full items-center ">
+          <h1 className="text-blue-400 font-bold  text-[0.9rem]">{sender}</h1>
+          <h2 className="text-[0.73rem] ml-2 text-gray-700 font-bold">
+            {date}
+          </h2>
+        </div>
+        <div className="mt-1">
+          {text && <p className="text-sm font-medium text-gray-200">{text}</p>}
+          {image && (
+            <img src={image} className="w-full h-auto mt-3 rounded-xl" />
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const AirdropSidebar = () => {
+    let [showchat, setShowChat] = useState(false);
   return (
     <div className="w-[20vw]  fixed invisible md:visible top-[70px] right-0 bottom-0 left-[80vw] bg-black  h-full ">
-        <div className="h-[calc(100vh-70px)] relative w-full  ">
-          <div className="absolute inset-0 backdrop:blur-3xl">
-            <button className="bg-blue-400  cursor-pointer hover:border-white transition-all border-[#017794] border-2 rounded-full py-2 px-3 text-white font-bold text-lg mt-10 ">
-              Connect Wallet
-            </button>
+      <div className="h-[calc(100vh-70px)] relative w-full pt-3 font-play">
+        <div className={`absolute  inset-0 z-50 ${showchat? "hidden" : "flex"}  justify-center items-center backdrop:blur-lg bg-black bg-opacity-30`}>
+          <button onClick={()=>setShowChat(true)} className=" cursor-pointer hover:border-white transition-all border-[#017794] border-2 rounded-full py-2 px-3 text-white font-bold text-lg mt-10 ">
+            Connect Wallet
+          </button>
+        </div>
+        <div className={`flex flex-col h-full  font-play ${showchat? "blur-0":"blur-md"} `}>
+          <div className='flex-grow  overflow-y-auto px-2  space-y-5'>
+            {chat.map((msg) => {
+              return <ChatMessage {...msg} />
+            })}
+          </div>
+          <div className="h-[10vh] bg-gray-900 w-full mt-4 flex items-center px-3 pr-6 justify-between">
+            <input type="text" className='bg-transparent text-gray-300 text-sm outline-none border-none font-medium' placeholder='Message # | the-arena'/>
+            <BsEmojiSmile className='text-white hover:text-yellow-500 cursor-pointer'/>
           </div>
         </div>
       </div>
+    </div>
   )
 }
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import participants from '../../utilts/participants'
+import activity from '../../utilts/activity'
 
 const TableEntry = ({ data }) => {
   return (
-    <tr className="hover:bg-gray-50">
-      <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
+    <tr className="hover:bg-gray-50 w-full flex">
+      <th className="flex gap-3 flex-grow px-6 py-4  items-center font-normal text-gray-900">
         <div className="relative h-10 w-10">
           <img
             className="h-full w-full rounded-full object-cover object-center"
@@ -11,9 +13,9 @@ const TableEntry = ({ data }) => {
             alt=""
           />
         </div>
-        <div className="text-sm">
+        <div className="text-sm flex flex-col items-center">
           <div className="font-medium text-gray-700">{data.name}</div>
-          <div className="text-gray-400 whitespace-nowrap">{data.entries}</div>
+          {data.entries && <div className="text-gray-400 whitespace-nowrap">{data.entries}</div>} 
         </div>
       </th>
 
@@ -32,21 +34,11 @@ const Table = () => {
   const tabData = [
     {
       label: 'ACTIVITY',
-      tableData: [
-        { name: 'AirBorns', entries: '50 entries', time: '1221 Entries' },
-        { name: 'AirBorns', entries: '50 entries', time: '21 Entries' },
-        { name: 'AirBorns', entries: '50 entries', time: '43 Entries' },
-        { name: 'AirBorns', entries: '50 entries', time: '1321 Entries' },
-      ],
+      tableData: activity,
     },
     {
       label: 'PARTICIPANTS',
-      tableData: [
-        { name: 'AirBorns', entries: null, time: '8 hours ago' },
-        { name: 'AirBorns', entries: null, time: '8 hours ago' },
-        { name: 'AirBorns', entries: null, time: '8 hours ago' },
-        { name: 'AirBorns', entries: null, time: '8 hours ago' },
-      ],
+      tableData: participants,
     },
   ]
 
@@ -57,9 +49,9 @@ const Table = () => {
   return (
     <>
       {/* component */}
-      <div className="overflow-x-hidden overflow-y-auto w-full rounded-lg border border-gray-200 shadow-md">
-        <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
-          <thead className='flex'>
+      <div className="overflow-x-hidden  w-full rounded-lg border border-gray-200 shadow-md">
+        <table className="w-full flex flex-col  border-collapse bg-white text-left text-sm text-gray-500">
+          <thead className='flex h-[8vh]'>
             {tabData.map((tab, index) => (
               <div
                 key={index}
@@ -72,7 +64,7 @@ const Table = () => {
               </div>
             ))}
           </thead>
-          <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+          <tbody className="divide-y transition-all w-full grid overflow-y-auto max-h-96 flex-grow divide-gray-100 border-t border-gray-100">
             {tabData[activeTab].tableData.map((entry, index) => (
               <TableEntry key={index} data={entry} />
             ))}
